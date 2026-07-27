41085ef867cd22dd5c8b206207d9cb8b0961b176
unknown <2607975205@qq.com>
2026-07-27 05:14:07 +0000

    build backend layere architecture

---
+++ transaction-monitoring-backend/src/main/java/org/example/transactionmonitoringbackend/TransactionMonitoringBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.transactionmonitoringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionMonitoringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionMonitoringBackendApplication.class, args);
+    }
+
+}
---
+++ transaction-monitoring-backend/src/test/java/org/example/transactionmonitoringbackend/TransactionMonitoringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.transactionmonitoringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionMonitoringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
