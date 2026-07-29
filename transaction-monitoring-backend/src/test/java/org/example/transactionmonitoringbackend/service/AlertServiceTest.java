1764f06860820cc2f67a307440af5f3c256da03f
Bright <BrightTsui@outlook.com>
2026-07-29 10:11:26 +0800

    test getAlertByID in AlertService

--- transaction-monitoring-backend/src/test/java/org/example/transactionmonitoringbackend/service/AlertServiceTest.java
+++ transaction-monitoring-backend/src/test/java/org/example/transactionmonitoringbackend/service/AlertServiceTest.java
@@ -1,6 +1,7 @@
 package org.example.transactionmonitoringbackend.service;
 
 import org.example.transactionmonitoringbackend.entity.Alert;
+import org.example.transactionmonitoringbackend.entity.AlertStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @Transactional
@@ -34,4 +36,25 @@ public class AlertServiceTest {
         List<Alert> after = alertService.getAllAlert();
         assertEquals(before.size() + 2, after.size());
     }
+
+    @Test
+    void getAlertByID() {
+        jdbcTemplate.update(
+                "INSERT INTO alerts(transaction_id, rule_id, status, created_at) values (?, ?, ?, ?)",
+                "955", 1, "OPEN", LocalDateTime.now()
+        );
+        Long id = jdbcTemplate.queryForObject(
+                "SELECT id FROM alerts WHERE transaction_id = ?",
+                Long.class,
+                955L
+        );
+        Alert alert = alertService.getAlertById(id);
+        assertNotNull(alert);
+        assertEquals(id, alert.getId());
+        assertEquals(955, alert.getTransactionId());
+        assertEquals(1, alert.getRuleId());
+        assertEquals("OPEN", alert.getStatus().name());
+    }
+
+
 }
